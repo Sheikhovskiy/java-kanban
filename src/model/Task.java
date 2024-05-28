f00ad506cc759d3b33cca40d753280164319110d
Sheikh <benabdallah@bk.ru>
2024-05-28 22:59:52 +0300

    Добавить сохранение изменений 1

--- src/model/Task.java
+++ src/model/Task.java
@@ -25,6 +25,11 @@ public class Task {
 
     }
 
+
+    public TaskType getType() {
+        return TaskType.TASK;
+    }
+
     public String getTaskName() { return taskName; }
 
     public void setTaskName(String taskName) { this.taskName = taskName; }
