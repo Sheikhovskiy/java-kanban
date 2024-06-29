41420aa73407da459547a0e1e3d00e84e65cbc4f
Sheikh <benabdallah@bk.ru>
2024-06-29 17:01:23 +0300

    добавить строки для фикса тестов #7

--- src/ownexceptions/NotFoundException.java
+++ src/ownexceptions/NotFoundException.java
@@ -3,6 +3,7 @@ package ownexceptions;
 public class NotFoundException extends RuntimeException {
 
     public NotFoundException(String message) {
-        super(message); }
+        super(message);
+    }
 }
 
